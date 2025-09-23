95ebad88d882b0ea1f60be75d5621add308111ae
magicat8 <magicat8bill@gmail.com>
2025-09-23 12:19:44 -0600

    Add price field and update copies fields in Book model

--- library_database/books/models.py
+++ library_database/books/models.py
@@ -13,10 +13,11 @@ class Book(models.Model):
     categories = models.CharField(max_length=200, blank=True, null=True)
     isbn = models.CharField(max_length=20, null=True, blank=True)
     copies = models.PositiveIntegerField(default=1)
-    hardback_copies = models.PositiveIntegerField(default=0)
-    papperback_copies = models.PositiveIntegerField(default=0)
-    new_copies = models.PositiveIntegerField(default=0)
-    used_copies = models.PositiveIntegerField(default=0)
+    price = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    hardback_copies = models.PositiveIntegerField(null=True, blank=True)
+    papperback_copies = models.PositiveIntegerField(null=True, blank=True)
+    new_copies = models.PositiveIntegerField(null=True, blank=True)
+    used_copies = models.PositiveIntegerField(null=True, blank=True)
 
     def __str__(self):
         return f"{self.title} by {self.author} (Copies: {self.copies})"

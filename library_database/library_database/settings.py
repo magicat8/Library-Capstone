dde358a03d3211c9c0640578e4456ae00fd19b38
magicat8bill <magicat8bill@gmail.com>
2025-10-06 11:45:49 -0600

    Add CSRF_TRUSTED_ORIGINS for secure requests from host.xeroideas.org

--- library_database/library_database/settings.py
+++ library_database/library_database/settings.py
@@ -128,3 +128,7 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://host.xeroideas.org'
+]
